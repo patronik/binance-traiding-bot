f3fbb80a0422a72ab462dfa59aaafa234415386b
Vasyl Voina <vasyl.voina@regiondo.com>
2024-08-13 11:24:21 +0300

    Minor improvements

--- public/editor.js
+++ public/editor.js
@@ -3,6 +3,36 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.session.setMode("ace/mode/javascript");
 
+let editorInitialContent = getCookie('editorContent');
+
+if (editorInitialContent != null) {
+  editorInitialContent = atob(editorInitialContent);
+} else {
+  editorInitialContent = `
+  /**
+    Variables available:
+    let currentBtcPrice; let currentBtcBalance; let currentUsdtBalance;
+
+    Html buttons available:
+    document.getElementById('sellAllBtn');
+    document.getElementById('sellSpecificBtn');
+    document.getElementById('clearFieldsBtn');
+    document.getElementById('buySpecificBtn');
+
+    Html elements available:
+    document.getElementById('buyAmount');
+    document.getElementById('sellAmount');
+    document.getElementById("executeCheckbox").checked;
+**/
+`;
+}
+
+editor.setValue(editorInitialContent);
+
+editor.on('input', () => {
+  setCookie('editorContent', btoa(editor.getValue()), 7);
+});
+
 // Function to execute code
 function runCallback()
 {
@@ -14,7 +44,5 @@ function runCallback()
     } catch (e) {
       console.error("Error executing code:", e);
     }
-  } else {
-    // console.log("Code execution is disabled.");
   }
 };
