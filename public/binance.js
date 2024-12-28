aa77946df50ba49011f0bea2007d7e55127cbcac
Vasyl Voina <vasyl.voina@gmail.com>
2024-12-28 09:51:34 +0200

    Fix rounding

--- public/binance.js
+++ public/binance.js
@@ -5,61 +5,84 @@ function subtractPercentage(amount, percentage) {
     return newAmount;
 }
 
+function roundOrderAmount(amount, precision) {
+    const amountParts = amount.toString().split('.');
+    if (amountParts.length != 2) {
+        alert(`Invalid amount ${amount}`);
+        return;
+    }
+
+    precision = parseInt(precision);
+    if (isNaN(precision)) {
+        alert(`Invalid precision ${precision}`);
+        return;
+    }
+
+    if (precision == 0) {
+        return parseFloat(amountParts[0]);
+    } else if (precision > 0) {
+        let fractionalPart = amountParts[1].substring(
+            0, 
+            Math.min(amountParts[1].length, precision)
+        );
+        return parseFloat(`${amountParts[0]}.${fractionalPart}`);
+    } else {
+        alert(`Invalid precision ${precision}`);
+        return;
+    }
+}
+
 async function getSymbolPrecision(symbol) {
     const response = await fetch(`/precision/${symbol}`);
-    const data = await response.json(); 
+    const {qtyPrecision, pricePrecision, minNotional} = await response.json(); 
 
-    const result = {
-        qtyPrecision: 0, 
-        pricePrecision: 0, 
-        minNot: parseFloat(data.minNotional)
-    };
+    const result = {qty: 0, price: 0, minNot: parseFloat(minNotional || 0)};
 
-    let qtyPrecisionParts = parseFloat(data.qtyPrecision).toString().split('.');
+    let qtyPrecisionParts = parseFloat(qtyPrecision).toString().split('.');
     if (qtyPrecisionParts.length == 2) {
-        result['qtyPrecision'] = qtyPrecisionParts[1].length;
+        result['qty'] = qtyPrecisionParts[1].length;
     } 
 
-    let pricePrecisionParts = parseFloat(data.pricePrecision).toString().split('.');
+    let pricePrecisionParts = parseFloat(pricePrecision).toString().split('.');
     if (pricePrecisionParts.length == 2) {
-        result['pricePrecision'] = pricePrecisionParts[1].length;
+        result['price'] = pricePrecisionParts[1].length;
     } 
 
     return result;
 }
 
 async function sellAll(symbol) {
-    let symbolTotalAmount = parseFloat(document.getElementById(`f-bal-${symbol}`).textContent);
-    if (isNaN(symbolTotalAmount)) {
+    let symbolOrderAmount = parseFloat(document.getElementById(`f-bal-${symbol}`).textContent);
+    if (isNaN(symbolOrderAmount)) {
         alert('Invalid asset amount.');  
         return;
     }
+
+    symbolOrderAmount = subtractPercentage(symbolOrderAmount, feePercent);     
+        
+    const precision = await getSymbolPrecision(symbol);
+    symbolOrderAmount = roundOrderAmount(symbolOrderAmount, precision.qty);
 
     const assetUSDTPrice = parseFloat(document.getElementById(`price-${symbol}`).textContent);
     if (isNaN(assetUSDTPrice)) {
         alert(`Invalid asset USDT price ${assetUSDTPrice}.`);  
         return;
-    }     
-        
-    symbolTotalAmount = subtractPercentage(symbolTotalAmount, feePercent);    
+    }    
 
-    const precision = await getSymbolPrecision(symbol);
-    symbolTotalAmount = symbolTotalAmount.toFixed(precision.qtyPrecision);
-
-    const USDTAmount = (assetUSDTPrice * symbolTotalAmount).toFixed(2);
-    if (USDTAmount < precision.minNot) {
-        alert(`Order amount ${USDTAmount} is less than min value ${precision.minNot}.`);  
+    const USDTOrderAmount = assetUSDTPrice * symbolOrderAmount;
+    if (USDTOrderAmount < precision.minNot) {
+        alert(`Order amount ${USDTOrderAmount} is less than min value ${precision.minNot}.`);  
         return;
     }
 
-    const confirmation = confirm(`Do you confirm selling ${symbolTotalAmount} of ${symbol} (${USDTAmount} USDT)?`);
+    const confirmation = confirm(`Do you confirm selling ${symbolOrderAmount} of ${symbol} (${USDTOrderAmount} USDT)?`);
 
     if (confirmation) {
         try {
             const response = await fetch('/sell-symbol', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ amount: symbolTotalAmount, symbol })
+                body: JSON.stringify({ amount: symbolOrderAmount, symbol })
             });
             const data = await response.json();
             alert(`Sell Response: ${JSON.stringify(data)}`);
@@ -91,7 +114,7 @@ async function buyForFixedUSDT(symbol) {
     buyAmount = subtractPercentage(buyAmount, feePercent);
 
     const precision = await getSymbolPrecision(symbol);
-    buyAmount = buyAmount.toFixed(precision.qtyPrecision);
+    buyAmount = roundOrderAmount(buyAmount, precision.qty);
 
     if (fixedUSDT < precision.minNot) {
         alert(`Order amount ${fixedUSDT} is less than min value ${precision.minNot}.`);  
@@ -134,23 +157,23 @@ async function buyUSDTPercent(symbol) {
         return;
     }    
     
-    const USDTAmount = (USDTBal / 100 * USDTPercent);
-    let buyAmount = parseFloat(USDTAmount / assetUSDTPrice);
+    const USDTOrderAmount = (USDTBal / 100 * USDTPercent);
+    let buyAmount = parseFloat(USDTOrderAmount / assetUSDTPrice);
     if (isNaN(buyAmount)) {
         alert(`Invalid amount to buy ${buyAmount}.`);  
         return;
     }    
 
     const precision = await getSymbolPrecision(symbol);
-    if (USDTAmount < precision.minNot) {
-        alert(`Order amount ${USDTAmount} is less than min value ${precision.minNot}.`);  
+    if (USDTOrderAmount < precision.minNot) {
+        alert(`Order amount ${USDTOrderAmount} is less than min value ${precision.minNot}.`);  
         return;
     }
 
     buyAmount = subtractPercentage(buyAmount, feePercent);
-    buyAmount = buyAmount.toFixed(precision.qtyPrecision);
+    buyAmount = roundOrderAmount(buyAmount, precision.qtyPrecision);
 
-    const confirmation = confirm(`Do you confirm buying ${buyAmount} of ${symbol} (for ${USDTAmount} USDT)?`);
+    const confirmation = confirm(`Do you confirm buying ${buyAmount} of ${symbol} (for ${USDTOrderAmount} USDT)?`);
 
     if (confirmation) {
         try {
